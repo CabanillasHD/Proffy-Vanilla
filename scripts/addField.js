26bc9d3dd01f975bb8e1886a21d308e72eeeeaae
Henrique C <cabanillas445@gmail.com>
2020-08-17 19:47:05 -0300

    Duplicate Fields with JS

---
+++ scripts/addField.js
@@ -0,0 +1,27 @@
+//Procurar o botao
+document.querySelector("#add-time")
+    .addEventListener('click', cloneField);
+// document.querySelector("#add-time").onclick = cloneField;
+//Quando clicar no botao
+
+//Executar uma ação
+function cloneField() {
+    console.log("Cheguei aqui");
+
+    //Duplicar os campos. Que campos ?
+    const newFieldContainer = document.querySelector(".schedule-item").cloneNode(true) //boolean: true or false
+
+    //limpar os campos, que campos ?
+    const fields = newFieldContainer.querySelectorAll('input')
+
+    // fields[0].value = ""
+    // fields[1].value = ""
+    
+    fields.forEach(function(field){
+        field.value = ""
+    })
+
+    //Colocar na página, onde ?
+    document.querySelector("#schedule-items").appendChild(newFieldContainer)
+
+}
